3af738fe20da06075d29e38d909eeb2e61fad15f
Muhammad Dzulfiqar <muhammaddzulfiqar03@gmail.com>
2026-01-14 14:14:49 +0700

    add jadwal-pelaksanaan page update

--- resources/views/pages/Bank Sampah/jadwal-pelaksanaan.blade.php
+++ resources/views/pages/Bank Sampah/jadwal-pelaksanaan.blade.php
@@ -2,6 +2,9 @@
 
 @section('title', 'Jadwal Pelaksanaan - Bank Sampah')
 
+@section('content', content: 'Manajemen Bank Sampah')
+@section('route', route('jadwal-pelaksanaan'))
+@section('sub-content', 'Data Jadwal Pelaksanaan Bank Sampah')
 
 @section('data-jadwalBankSampah')
 
