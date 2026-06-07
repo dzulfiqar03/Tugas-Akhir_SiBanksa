4c6b830fc4036d5900123b8ea3dcca247d6a9a2e
Muhammad Dzulfiqar <muhammaddzulfiqar03@gmail.com>
2026-06-07 07:34:21 +0700

    Deploymant

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,15 +1,17 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8008,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8008,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'ap1',
+    wsHost: `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: 80,
+    wssPort: 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
+
 export default window.Echo;
